cc3a079336271c60950f2d0bfc502a9afa4b1e3d
Ilija Trokovski <ilija.trokovski@gmail.com>
2022-11-07 12:49:53 +0100

    Submit

---
+++ src/components/App.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useState } from "react";
+
+function NameApp() {
+  const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick(event) {
+    setHeading(name);
+
+    event.preventDefault();
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button
+          style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default NameApp;
